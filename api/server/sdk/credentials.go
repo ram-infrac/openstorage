d95625e60f99d7bdda753fbc962c496ebe268b8d
Ram <ram.suradkar@portworx.com>
2018-05-31 09:36:19 +0000

    Adding enumerate api for google, azure
     - cleanup enumerate parsing for credentials
     - added mock test cases for same
    
    Signed-off-by: Ram <ram.suradkar@portworx.com>

--- api/server/sdk/credentials.go
+++ api/server/sdk/credentials.go
@@ -19,141 +19,13 @@ package sdk
 
 import (
 	"context"
+	"errors"
 	"reflect"
 
-	"fmt"
 	"github.com/libopenstorage/openstorage/api"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-func (s *VolumeServer) ProvideForAWS(
-	ctx context.Context,
-	req *api.ProvideCredentialsForAWSRequest,
-) (*api.ProvideCredentialsForAWSResponse, error) {
-
-	if len(req.GetAccessKey()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Access Key")
-	}
-
-	if len(req.GetSecretKey()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Secret Key")
-	}
-
-	if len(req.GetRegion()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Region Key")
-	}
-
-	if len(req.GetEndpoint()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Endpoint Key")
-	}
-
-	params := make(map[string]string)
-
-	params[api.OptCredType] = "s3" //req.GetCredType()
-	params[api.OptCredRegion] = req.GetRegion()
-	params[api.OptCredEndpoint] = req.GetEndpoint()
-	params[api.OptCredAccessKey] = req.GetAccessKey()
-	params[api.OptCredSecretKey] = req.GetSecretKey()
-
-	uuid, err := s.driver.CredsCreate(params)
-
-	if err != nil {
-		return nil, status.Errorf(
-			codes.Internal,
-			"failed to create AWS S3 credentials: %v",
-			err.Error())
-	}
-
-	// Validate if the credentials provided were correct or not
-	validateReq := &api.CredentialsValidateRequest{CredentialId: uuid}
-
-	err = s.driver.CredsValidate(validateReq.GetCredentialId())
-
-	if err != nil {
-		deleteCred := &api.CredentialsDeleteRequest{CredentailId: uuid}
-		err = s.driver.CredsDelete(deleteCred.GetCredentailId())
-
-		if err != nil {
-			return nil, status.Errorf(
-				codes.Internal,
-				"failed to delete AWS S3 credentials: %v",
-				err.Error())
-		}
-	}
-	return &api.ProvideCredentialsForAWSResponse{CredentialId: uuid}, nil
-
-}
-
-func (s *VolumeServer) ProvideForAzure(
-	ctx context.Context,
-	req *api.ProvideCredentialsForAzureRequest,
-) (*api.ProvideCredentialsForAzureResponse, error) {
-
-	if len(req.GetAccountKey()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Account Key")
-	}
-
-	if len(req.GetAccountName()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Account name")
-	}
-
-	params := make(map[string]string)
-
-	params[api.OptCredType] = "azure"
-	params[api.OptCredAzureAccountKey] = req.GetAccountKey()
-	params[api.OptCredAzureAccountName] = req.GetAccountName()
-
-	uuid, err := s.driver.CredsCreate(params)
-
-	if err != nil {
-		return nil, status.Errorf(
-			codes.Internal,
-			"failed to create Azure credentials: %v",
-			err.Error())
-	}
-
-	return &api.ProvideCredentialsForAzureResponse{CredentialId: uuid}, nil
-}
-
-func (s *VolumeServer) ProvideForGoogle(
-	ctx context.Context,
-	req *api.ProvideCredentialsForGoogleRequest,
-) (*api.ProvideCredentialsForGoogleResponse, error) {
-
-	if len(req.GetJsonKey()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply JSON Key")
-	}
-
-	if len(req.GetProjectId()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Must supply Project ID")
-	}
-
-	params := make(map[string]string)
-
-	params[api.OptCredType] = "google" //req.GetCredType()
-	params[api.OptCredGoogleProjectID] = req.GetProjectId()
-	params[api.OptCredGoogleJsonKey] = req.GetJsonKey()
-
-	uuid, err := s.driver.CredsCreate(params)
-
-	if err != nil {
-		return nil, status.Errorf(
-			codes.Internal,
-			"failed to create Google credentials: %v",
-			err.Error())
-	}
-	return &api.ProvideCredentialsForGoogleResponse{CredentialId: uuid}, nil
-}
-
-func (s *VolumeServer) CredentialsValidate(
-	ctx context.Context,
-	req *api.CredentialsValidateRequest,
-) (*api.CredentialsValidateResponse, error) {
-
-	return &api.CredentialsValidateResponse{}, nil
-
-}
 
 // Delete specfied credentials
 func (s *VolumeServer) CredentialsDelete(
@@ -176,13 +48,34 @@ func (s *VolumeServer) CredentialsDelete(
 	return &api.CredentialsDeleteResponse{}, nil
 }
 
+func getCredentialMap(credList map[string]interface{}, credType string) (map[string]interface{}, error) {
+	creds := make(map[string]interface{})
+
+	for k, v := range credList {
+		c, ok := v.(map[string]interface{})
+		if !ok {
+			return nil, errors.New("Error parsing credentials %v" +
+				reflect.TypeOf(v).String())
+		}
+
+		// Look for only one type
+		switch c[api.OptCredType] {
+		case credType:
+			creds[k] = v
+		default:
+			return nil, errors.New("Could not find credentials stored for " + credType)
+		}
+	}
+
+	return creds, nil
+}
+
 // EnumerateCredentialsForAWS list credentials for AWS
 func (s *VolumeServer) EnumerateForAWS(
 	ctx context.Context,
 	req *api.EnumerateCredentialsForAWSRequest,
 ) (*api.EnumerateCredentialsForAWSResponse, error) {
 
-	s3Creds := make(map[string]interface{})
 	credList, err := s.driver.CredsEnumerate()
 	if err != nil {
 		return nil, status.Errorf(
@@ -195,29 +88,16 @@ func (s *VolumeServer) EnumerateForAWS(
 	// respective cloud provider and return result
 	// this may not be expected behaviour, we have to do this since
 	// `interface` can't be mapped directly with other lang
-
-	// First separate them into the type
-	for k, v := range credList {
-		c, ok := v.(map[string]interface{})
-		if !ok {
-			return nil, status.Errorf(
-				codes.Internal,
-				"Error parsing credentials %v",
-				reflect.TypeOf(v).String())
-		}
-		fmt.Println("c", c)
-		switch c["type"] {
-		case "s3":
-			s3Creds[k] = v
-		default:
-			return nil, status.Errorf(
-				codes.Internal,
-				"Didn't find AWS credential stored %v",
-				reflect.TypeOf(v).String())
-		}
+	s3Creds, err := getCredentialMap(credList, "s3")
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to enumerate credentials AWS: %v",
+			err.Error())
 	}
 
-	creds := []*api.Credential{}
+	// Fill up s3 credential resonse
+	creds := []*api.S3Credential{}
 	for k, v := range s3Creds {
 		cred, ok := v.(map[string]interface{})
 		if !ok {
@@ -227,11 +107,11 @@ func (s *VolumeServer) EnumerateForAWS(
 				reflect.TypeOf(v).String())
 		}
 
-		credResp := &api.Credential{
+		credResp := &api.S3Credential{
 			CredentialId: k,
-			AccessKey:    cred["accesskey"].(string),
-			Endpoint:     cred["endpoint"].(string),
-			Region:       cred["region"].(string),
+			AccessKey:    cred[api.OptCredAccessKey].(string),
+			Endpoint:     cred[api.OptCredEndpoint].(string),
+			Region:       cred[api.OptCredRegion].(string),
 		}
 		creds = append(creds, credResp)
 	}
@@ -244,7 +124,45 @@ func (s *VolumeServer) EnumerateForAzure(
 	ctx context.Context,
 	req *api.EnumerateCredentialsForAzureRequest,
 ) (*api.EnumerateCredentialsForAzureResponse, error) {
-	return &api.EnumerateCredentialsForAzureResponse{}, nil
+	credList, err := s.driver.CredsEnumerate()
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to enumerate credentials: %v",
+			err.Error())
+	}
+
+	// By defaultcredList will have all credential details, we will extract for
+	// respective cloud provider and return result
+	// this may not be expected behaviour, we have to do this since
+	// `interface` can't be mapped directly with other lang
+	azureCreds, err := getCredentialMap(credList, "azure")
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to enumerate credentials Azure: %v",
+			err.Error())
+	}
+
+	// Fill up azure credential resonse
+	creds := []*api.AzureCredential{}
+	for k, v := range azureCreds {
+		cred, ok := v.(map[string]interface{})
+		if !ok {
+			return nil, status.Errorf(
+				codes.Internal,
+				"Unable to enumerate credentials AWS: %v",
+				reflect.TypeOf(v).String())
+		}
+
+		credResp := &api.AzureCredential{
+			CredentialId: k,
+			AccountName:  cred[api.OptCredAzureAccountName].(string),
+			AccountKey:   cred[api.OptCredAzureAccountKey].(string),
+		}
+		creds = append(creds, credResp)
+	}
+	return &api.EnumerateCredentialsForAzureResponse{Cred: creds}, nil
 }
 
 // EnumerateCredentialsForAWS list credentials for Google
@@ -252,5 +170,43 @@ func (s *VolumeServer) EnumerateForGoogle(
 	ctx context.Context,
 	req *api.EnumerateCredentialsForGoogleRequest,
 ) (*api.EnumerateCredentialsForGoogleResponse, error) {
-	return &api.EnumerateCredentialsForGoogleResponse{}, nil
+	credList, err := s.driver.CredsEnumerate()
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to enumerate credentials: %v",
+			err.Error())
+	}
+
+	// By defaultcredList will have all credential details, we will extract for
+	// respective cloud provider and return result
+	// this may not be expected behaviour, we have to do this since
+	// `interface` can't be mapped directly with other lang
+	googleCreds, err := getCredentialMap(credList, "google")
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to enumerate credentials Azure: %v",
+			err.Error())
+	}
+
+	// Fill up google credential resonse
+	creds := []*api.GoogleCredential{}
+	for k, v := range googleCreds {
+		cred, ok := v.(map[string]interface{})
+		if !ok {
+			return nil, status.Errorf(
+				codes.Internal,
+				"Unable to enumerate credentials AWS: %v",
+				reflect.TypeOf(v).String())
+		}
+
+		credResp := &api.GoogleCredential{
+			CredentialId: k,
+			ProjectId:    cred[api.OptCredGoogleProjectID].(string),
+		}
+		creds = append(creds, credResp)
+	}
+
+	return &api.EnumerateCredentialsForGoogleResponse{Cred: creds}, nil
 }
--- api/server/sdk/credentials_test.go
+++ api/server/sdk/credentials_test.go
@@ -26,120 +26,23 @@ import (
 	"github.com/libopenstorage/openstorage/api"
 )
 
-func TestSdkCredentialCreateSuccess(t *testing.T) {
-
-	// Create server and client connection
-	s := newTestServer(t)
-	defer s.Stop()
-
-	req := &api.ProvideCredentialsForAWSRequest{
-		CredType:  "s3",
-		AccessKey: "dummy-access",
-		SecretKey: "dummy-secret",
-		Endpoint:  "dummy-endpoint",
-		Region:    "dummy-region",
-	}
-
-	params := make(map[string]string)
-
-	params[api.OptCredType] = req.GetCredType()
-	params[api.OptCredRegion] = req.GetRegion()
-	params[api.OptCredEndpoint] = req.GetEndpoint()
-	params[api.OptCredAccessKey] = req.GetAccessKey()
-	params[api.OptCredSecretKey] = req.GetSecretKey()
-
-	uuid := "good-uuid"
-	s.MockDriver().
-		EXPECT().
-		CredsCreate(params).
-		Return(uuid, nil)
-
-	s.MockDriver().
-		EXPECT().
-		CredsValidate(uuid).
-		Return(nil)
-
-		// Setup client
-	c := api.NewOpenStorageCredentialsClient(s.Conn())
-
-	// Attach Volume
-	_, err := c.ProvideForAWS(context.Background(), req)
-	assert.NoError(t, err)
-}
-
-func TestSdkCredentialCreateFailed(t *testing.T) {
-
-	// Create server and client connection
-	s := newTestServer(t)
-	defer s.Stop()
-
-	req := &api.ProvideCredentialsForAWSRequest{
-		CredType:  "s3",
-		AccessKey: "dummy-access",
-		SecretKey: "dummy-secret",
-		Endpoint:  "dummy-endpoint",
-		Region:    "dummy-region",
-	}
-
-	params := make(map[string]string)
-
-	params[api.OptCredType] = req.GetCredType()
-	params[api.OptCredRegion] = req.GetRegion()
-	params[api.OptCredEndpoint] = req.GetEndpoint()
-	params[api.OptCredAccessKey] = req.GetAccessKey()
-	params[api.OptCredSecretKey] = req.GetSecretKey()
-
-	uuid := "bad-uuid"
-	s.MockDriver().
-		EXPECT().
-		CredsCreate(params).
-		Return(uuid, nil)
-
-	s.MockDriver().
-		EXPECT().
-		CredsValidate(uuid).
-		Return(fmt.Errorf("Invalid credentials"))
-
-	s.MockDriver().
-		EXPECT().
-		CredsDelete(uuid).
-		Return(nil)
-
-		// Setup client
-	c := api.NewOpenStorageCredentialsClient(s.Conn())
-
-	// Attach Volume
-	_, err := c.ProvideForAWS(context.Background(), req)
-	assert.NoError(t, err)
-}
-
-func TestSdkCredentialEnumerateSuccess(t *testing.T) {
+func TestSdkCredentialEnumerateAWSSuccess(t *testing.T) {
 
 	// Create server and client connection
 	s := newTestServer(t)
 	defer s.Stop()
 
 	req := &api.EnumerateCredentialsForAWSRequest{CredentialId: "test"}
-	/*	resp := []*api.EnumerateCredentialsForAWSResponse{
-			&api.Credential{
-				CredentialId: "s3",
-				AccessKey:    "dummy-access",
-				SecretKey:    "dummy-secret",
-				Endpoint:     "dummy-endpoint",
-				Region:       "dummy-region",
-			},
-		}
-	*/
 
 	enumS3 := map[string]interface{}{
-		"type":      "s3",
-		"accesskey": "dummy-access",
-		"SecretKey": "dummy-secret",
-		"endpoint":  "dummy-endpoint",
-		"region":    "dummy-region",
+		api.OptCredType:      "s3",
+		api.OptCredAccessKey: "test-access",
+		api.OptCredSecretKey: "test-secret",
+		api.OptCredEndpoint:  "test-endpoint",
+		api.OptCredRegion:    "test-region",
 	}
 	enumerateData := map[string]interface{}{
-		"type": enumS3,
+		api.OptCredUUID: enumS3,
 	}
 
 	s.MockDriver().
@@ -151,6 +54,213 @@ func TestSdkCredentialEnumerateSuccess(t *testing.T) {
 	c := api.NewOpenStorageCredentialsClient(s.Conn())
 
 	// Attach Volume
-	_, err := c.EnumerateForAWS(context.Background(), req)
+	resp, err := c.EnumerateForAWS(context.Background(), req)
 	assert.NoError(t, err)
+
+	assert.Equal(t, resp.GetCred()[0].AccessKey, enumS3[api.OptCredAccessKey])
+	assert.Equal(t, resp.GetCred()[0].Endpoint, enumS3[api.OptCredEndpoint])
+}
+
+func TestSdkCredentialEnumerateAWSFailed(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	req := &api.EnumerateCredentialsForAWSRequest{CredentialId: "test"}
+
+	s.MockDriver().
+		EXPECT().
+		CredsEnumerate().
+		Return(nil, fmt.Errorf("Failed to get credenntials details"))
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// EnumerateCredentials for AWS
+	resp, err := c.EnumerateForAWS(context.Background(), req)
+
+	assert.Nil(t, resp)
+	assert.Error(t, err)
+
+}
+
+func TestSdkCredentialEnumerateAzureSuccess(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	req := &api.EnumerateCredentialsForAzureRequest{CredentialId: "test"}
+
+	enumAzure := map[string]interface{}{
+		api.OptCredType:             "azure",
+		api.OptCredAzureAccountName: "test-azure-account",
+		api.OptCredAzureAccountKey:  "test-azure-account",
+	}
+	enumerateData := map[string]interface{}{
+		api.OptCredUUID: enumAzure,
+	}
+
+	s.MockDriver().
+		EXPECT().
+		CredsEnumerate().
+		Return(enumerateData, nil)
+
+		// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// Attach Volume
+	resp, err := c.EnumerateForAzure(context.Background(), req)
+	assert.NoError(t, err)
+
+	assert.Equal(t, resp.GetCred()[0].AccountName, enumAzure[api.OptCredAzureAccountName])
+	assert.Equal(t, resp.GetCred()[0].AccountKey, enumAzure[api.OptCredAzureAccountKey])
+
+}
+
+func TestSdkCredentialEnumerateAzureFailed(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	req := &api.EnumerateCredentialsForAzureRequest{CredentialId: "test"}
+
+	s.MockDriver().
+		EXPECT().
+		CredsEnumerate().
+		Return(nil, fmt.Errorf("Failed to get credenntials details"))
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// EnumerateCredentials for AWS
+	resp, err := c.EnumerateForAzure(context.Background(), req)
+
+	assert.Nil(t, resp)
+	assert.Error(t, err)
+}
+
+func TestSdkCredentialEnumerateGoogleSuccess(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	req := &api.EnumerateCredentialsForGoogleRequest{CredentialId: "test"}
+
+	enumGoogle := map[string]interface{}{
+		api.OptCredType:            "google",
+		api.OptCredGoogleProjectID: "test-google-project-id",
+	}
+	enumerateData := map[string]interface{}{
+		api.OptCredUUID: enumGoogle,
+	}
+
+	s.MockDriver().
+		EXPECT().
+		CredsEnumerate().
+		Return(enumerateData, nil)
+
+		// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// Attach Volume
+	resp, err := c.EnumerateForGoogle(context.Background(), req)
+	assert.NoError(t, err)
+
+	assert.Equal(t, resp.GetCred()[0].ProjectId, enumGoogle[api.OptCredGoogleProjectID])
+}
+
+func TestSdkCredentialEnumerateGoogleFailed(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	req := &api.EnumerateCredentialsForGoogleRequest{CredentialId: "test"}
+
+	s.MockDriver().
+		EXPECT().
+		CredsEnumerate().
+		Return(nil, fmt.Errorf("Failed to get credenntials details"))
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// EnumerateCredentials for AWS
+	resp, err := c.EnumerateForGoogle(context.Background(), req)
+
+	assert.Nil(t, resp)
+	assert.Error(t, err)
+}
+
+func TestSdkCredentialsDeleteSuccess(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	cred_id := "myid"
+	req := &api.CredentialsDeleteRequest{
+		CredentailId: cred_id,
+	}
+	s.MockDriver().
+		EXPECT().
+		CredsDelete(cred_id).
+		Return(nil)
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// Delete Credentials
+	_, err := c.CredentialsDelete(context.Background(), req)
+	assert.NoError(t, err)
+}
+
+func TestSdkCredentialsDeleteBadArgument(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	cred_id := ""
+	req := &api.CredentialsDeleteRequest{
+		CredentailId: cred_id,
+	}
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// Delete Credentials
+	_, err := c.CredentialsDelete(context.Background(), req)
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Must provide credentials uuid")
+}
+
+func TestSdkCredentialsDeleteFailed(t *testing.T) {
+
+	// Create server and client connection
+	s := newTestServer(t)
+	defer s.Stop()
+
+	cred_id := "myid"
+	req := &api.CredentialsDeleteRequest{
+		CredentailId: cred_id,
+	}
+	s.MockDriver().
+		EXPECT().
+		CredsDelete(cred_id).
+		Return(fmt.Errorf("Error deleting credentials"))
+
+	// Setup client
+	c := api.NewOpenStorageCredentialsClient(s.Conn())
+
+	// Delete Credentials
+	_, err := c.CredentialsDelete(context.Background(), req)
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Error deleting credentials")
 }
